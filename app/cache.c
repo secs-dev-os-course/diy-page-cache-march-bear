9e052116f77e0fa84c2eec92ab7790f3b63abd3a
Ivan Marukhlenko <ivan.marukh@gmail.com>
2025-02-14 00:58:05 +0300

    Интерфейс cache адаптирован под использование page_key и ioff; Исправления реализации функций

--- app/cache.c
+++ app/cache.c
@@ -3,10 +3,10 @@
 
 #include "cache.h"
 
-ssize_t cache_get_size(struct cache_list* cache, int fd, off_t offset) {
+ssize_t cache_get_size(struct cache_list* cache, page_key key) {
     struct cache_node* curr = cache->first;
     for (size_t _i = 0; _i < cache->length; ++_i) {
-        if (cache_node_is(curr, fd, offset)) {
+        if (cache_node_is(curr, key)) {
             return curr->size;
         }
     }
@@ -14,15 +14,21 @@ ssize_t cache_get_size(struct cache_list* cache, int fd, off_t offset) {
     return -1;
 }
 
-int cache_get(struct cache_list* cache, int fd, off_t offset, void* buf, size_t count) {
+// читает данные из ноды (если существует) в буфер, если count больше размера блока - ошибка
+int cache_get(struct cache_list* cache, page_key key, off_t ioff, void* buf, size_t count) {
     struct cache_node* curr = cache->first;
     for (size_t _i = 0; _i < cache->length; ++_i) {
-        if (cache_node_is(curr, fd, offset)) {
-            if (count < curr->size) {
-                return CACHE_ERROR_TOO_SMALL_BUF;
+        if (cache_node_is(curr, key)) {
+            if (count > curr->size) {
+                return CACHE_ERROR_TOO_BIG_COUNT;
             }
+
+            if (ioff < curr->ioff) {
+                return CACHE_NO_DATA_BEFORE_IOFF;
+            }
+
             curr->chance = true;
-            memcpy(buf, curr->data, curr->size);
+            memcpy(buf, curr->data + ioff, count);
             return CACHE_NO_ERROR;
         }
     }
@@ -30,24 +36,33 @@ int cache_get(struct cache_list* cache, int fd, off_t offset, void* buf, size_t 
     return CACHE_ERROR_NO_BLOCK;
 }
 
-int cache_update(struct cache_list* cache, int fd, off_t offset, void* data_buf, size_t count) {
+int cache_update(
+    struct cache_list* cache, page_key key, bool to_wr, off_t ioff, void* data_buf, size_t count
+) {
+    if (ioff + count > CACHE_NODE_DATA_CAP) {
+        return CACHE_ERROR_TOO_BIG_COUNT;
+    }
+
     struct cache_node* curr = cache->first;
     for (size_t _i = 0; _i < cache->length; ++_i) {
-        if (cache_node_is(curr, fd, offset)) {
+        if (cache_node_is(curr, key)) {
             curr->chance = true;
-            size_t data_buf_size = min(count, CACHE_NODE_DATA_CAP);
 
-            if (curr->size != data_buf_size) {
-                void* new_data = realloc(curr->data, data_buf_size);
+            if (curr->size + curr->ioff < count + ioff) {
+                void* new_data = realloc(curr->data, count + ioff);
                 if (new_data == NULL) {
                     return CACHE_ERROR_REALLOC_DATA;
                 }
-
-                curr->size = data_buf_size;
+                
                 curr->data = new_data;
             }
+            off_t min_ioff = min(ioff, curr->ioff);
+            curr->size = max(count + ioff - min_ioff, curr->size + curr->ioff - min_ioff);
+            curr->ioff = min_ioff;
 
-            memcpy(curr->data, data_buf, data_buf_size);
+            curr->sync = !to_wr;
+
+            memcpy(curr->data + ioff, data_buf, count);
 
             return CACHE_NO_ERROR;
         }
@@ -57,18 +72,18 @@ int cache_update(struct cache_list* cache, int fd, off_t offset, void* data_buf,
 }
 
 struct cache_node* cache_put(
-    struct cache_list* cache, int fd, off_t offset, void* data_buf, size_t count
+    struct cache_list* cache, page_key key, bool to_wr, off_t ioff, void* data_buf, size_t count
 ) {
-    if (cache_update(cache, fd, offset, data_buf, count) != NULL) {
+    if (cache_update(cache, key, to_wr, ioff, data_buf, count) == CACHE_NO_ERROR) {
         return NULL;
     }
 
-    struct cache_node* removed;
+    struct cache_node* removed = NULL;
     if (cache->length == cache->capacity) {
         removed = cache_list_remove_oldest(cache);
     }
 
-    if (cache_list_add_back(cache, fd, offset, data_buf, count) == NULL) {
+    if (cache_list_add_back(cache, key, !to_wr, ioff, data_buf, count) == NULL) {
         cache_node_free(removed);
         return &error_node;
     }
--- app/cache.h
+++ app/cache.h
@@ -8,8 +8,16 @@
 #define CACHE_NO_ERROR 0
 #define CACHE_ERROR_NO_BLOCK -1
 #define CACHE_ERROR_REALLOC_DATA -2
-#define CACHE_ERROR_TOO_SMALL_BUF -3
+#define CACHE_ERROR_TOO_BIG_COUNT -3
+#define CACHE_NO_DATA_BEFORE_IOFF -4
 
-ssize_t cache_get_size(struct cache_list* cache, int fd, off_t offset);
-int cache_get(struct cache_list* cache, int fd, off_t offset, void* buf, size_t count);
-struct cache_node* cache_put(struct cache_list* cache, int fd, off_t offset, void* data_buf, size_t count);
+/*
+    Работа с кэшем на уровне "положить/получить"
+    Интерфейс к cache_list
+*/
+
+ssize_t cache_get_size(struct cache_list* cache, page_key key);
+int cache_get(struct cache_list* cache, page_key key, off_t ioff, void* buf, size_t count);
+struct cache_node* cache_put(
+    struct cache_list* cache, page_key key, bool to_wr, off_t ioff, void* data_buf, size_t count
+);
